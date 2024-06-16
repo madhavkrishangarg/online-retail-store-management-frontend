53be24d6e6376c44af265e1f6f94202de1cd974f
madhavkrishangarg <108024625+madhavkrishangarg@users.noreply.github.com>
2024-06-17 00:58:06 +0530

    chore: Update dependencies and CSS for user and admin dashboards

--- src/components/UserDashboard.js
+++ src/components/UserDashboard.js
@@ -3,12 +3,13 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import './UserDashboard.css';
 import Search from './Search';
-import SearchResults from './SearchResults';
 
 function UserDashboard() {
     const [cart, setCart] = useState([]);
     const [orders, setOrders] = useState([]);
     const [products, setProducts] = useState([]);
+    const [currentView, setCurrentView] = useState('search');
+    const [productQuantities, setProductQuantities] = useState({});
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -68,6 +69,10 @@ function UserDashboard() {
     };
 
     const handleAddToCart = async (productId, quantity) => {
+        if(quantity < 1) {
+            alert('Quantity should be at least 1');
+            return;
+        }
         try {
             const userId = localStorage.getItem('userID');
             await axios.post(`/cart/${userId}/${productId}`, { quantity });
@@ -100,9 +105,29 @@ function UserDashboard() {
         }
     };
 
+    const handleLogout = () => {
+        localStorage.removeItem('userID');
+        navigate('/user-login');
+    };
+
+    const handleQuantityChange = (productId, quantity) => {
+        setProductQuantities((prevProductQuantities) => ({
+            ...prevProductQuantities,
+            [productId]: quantity,
+        }));
+    }
+
     return (
         <div className="user-dashboard-container">
             <div className="user-dashboard-card">
+                <div className="left-buttons">
+                    <button className="logout-button" onClick={handleLogout}>
+                        <i className="fas fa-sign-out-alt"></i>
+                    </button>
+                    <button className="cart-button" onClick={() => setCurrentView('cart')}>
+                        <i className="fas fa-shopping-cart"></i>
+                    </button>
+                </div>
                 <h2>User Dashboard</h2>
 
                 <div className="search-section">
@@ -113,37 +138,51 @@ function UserDashboard() {
                     <ul className="product-list">
                         {products.map((product) => (
                             <li key={product.productID} className="product-item">
-                                {product.product_name} - ${product.price}
-                                <button onClick={() => handleAddToCart(product.productID, 1)} className="user-button">Add to Cart</button>
+                                {product.product_name} - Price: Rs.{product.price}
+                                <div>
+                                    <input
+                                        type="number"
+                                        min="1"
+                                        // value={productQuantities[product.productID] || 1}
+                                        onChange={(e) => handleQuantityChange(product.productID, e.target.value)}
+                                        className="quantity-input"
+                                    />
+                                    <button onClick={() => handleAddToCart(product.productID, productQuantities[product.productID])}
+                                        className="user-button">Add to Cart</button>
+                                </div>
                             </li>
                         ))}
                     </ul>
                 </div>
 
-                <div className="user-section">
-                    <h3>Your Cart</h3>
-                    <ul className="cart-list">
-                        {cart.map((item) => (
-                            <li key={item.productID} className="cart-item">
-                                {item.product_name} - {item.quantity}
-                                <button onClick={() => handleUpdateCart(item.productID, item.quantity + 1)} className="user-button">+</button>
-                                <button onClick={() => handleUpdateCart(item.productID, item.quantity - 1)} className="user-button">-</button>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
+                {currentView === 'cart' && (
+                    <div className="user-section">
+                        <h3>Your Cart</h3>
+                        <ul className="cart-list">
+                            {cart.map((item) => (
+                                <li key={item.productID} className="cart-item">
+                                    {item.product_name} - {item.quantity}
+                                    <button onClick={() => handleUpdateCart(item.productID, item.quantity + 1)} className="user-button">+</button>
+                                    <button onClick={() => handleUpdateCart(item.productID, item.quantity - 1)} className="user-button">-</button>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                )}
 
-                <div className="user-section">
-                    <h3>Your Orders</h3>
-                    <ul className="order-list">
-                        {orders.map((order, index) => (
-                            <li key={index} className="order-item">
-                                {order.product_name} - {order.quantity}
-                                <button onClick={() => handleCancelOrder(order.orderID)} className="user-button">Cancel Order</button>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
+                {currentView === 'orders' && (
+                    <div className="user-section">
+                        <h3>Your Orders</h3>
+                        <ul className="order-list">
+                            {orders.map((order, index) => (
+                                <li key={index} className="order-item">
+                                    {order.product_name} - {order.quantity}
+                                    <button onClick={() => handleCancelOrder(order.orderID)} className="user-button">Cancel Order</button>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                )}
             </div>
         </div>
     );
