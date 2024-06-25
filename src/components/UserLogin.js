723ce9a2dd2e0c516c18c9d776bebc0ef5c29fbe
madhavkrishangarg <108024625+madhavkrishangarg@users.noreply.github.com>
2024-06-26 02:14:03 +0530

    changed transport layer protocol to https

--- src/components/UserLogin.js
+++ src/components/UserLogin.js
@@ -12,7 +12,7 @@ function UserLogin() {
     const handleLogin = async () => {
 
         try {
-            const response = await axios.post(`http://${process.env.REACT_APP_DOMAIN}/api/auth_cust`, { email, password });
+            const response = await axios.post(`https://${process.env.REACT_APP_DOMAIN}/api/auth_cust`, { email, password });
             if (response.data.userID > 0) {
                 localStorage.setItem('userID', response.data.userID); // Store userID in localStorage
                 navigate('/user-dashboard');
